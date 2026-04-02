97eb3271c80877028a2310540f54204e8f4f58bd
HalanNSilva <halansilvanathanael@gmail.com>
2026-04-02 16:55:38 -0300

    feat: Laço de repetição Hello and Bye World escolhido

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,6 @@
-for i in range(1,151):
+repeticoes = input("Quantas vezes tu que q repiota essa merda?")
+repeticoes = int(repeticoes)
+for i in range(1,repeticoes + 1):
     if i % 2 != 0:
         print("Hello World")
     else:
