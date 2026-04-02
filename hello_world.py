fe0d5cef239923defd2c1594edcc310754ec545b
HalanNSilva <149614993+HalanNSilva@users.noreply.github.com>
2026-04-02 16:56:40 -0300

    feat: Laço de repetição indeterminado (#4)
    * feat: Laço de repetição indeterminado
    
    * feat: Laço de repetição indeterminado
    
    * feat: Laço de repetição Hello and Bye World
    
    * feat: Laço de repetição Hello and Bye World escolhido

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,7 @@
-print("Hello World")
+repeticoes = input("Quantas vezes tu que q repiota essa merda?")
+repeticoes = int(repeticoes)
+for i in range(1,repeticoes + 1):
+    if i % 2 != 0:
+        print("Hello World")
+    else:
+        print("Bye world")
