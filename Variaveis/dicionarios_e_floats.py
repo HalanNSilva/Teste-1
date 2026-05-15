7eadc0785f611d7e8bebc7ca1c9555ceec781009
HalanNSilva <halansilvanathanael@gmail.com>
2026-05-15 15:57:12 -0300

    feat: created dicionarios e floats

---
+++ Variaveis/dicionarios_e_floats.py
@@ -0,0 +1,12 @@
+Dict1 = {
+    "Laranja" : 10.50,
+    "Maçã" : 6.70,
+    "pera" : 8.90,
+    "Cereja" : 20.00,
+    "Melancia" : 33.33,
+}
+A = input("Futa 1: ")
+B = input("Fruta 2: ")
+
+soma = Dict1[A] + Dict1[B]
+print(soma)
